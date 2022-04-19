88d353a9e1a076dd937807b41fa85f20c7ae0755
devnull-twitch <97809736+devnull-twitch@users.noreply.github.com>
2022-04-19 20:56:03 +0200

    fix: add some more testing to target scene

--- Start.gd
+++ Start.gd
@@ -2,11 +2,12 @@ extends Node
 
 var http_request_node
 var http_downloader_node
-var scene_file
+var resource_pack_file
+var main_scene_file
 
 func _ready():
 	# Maybe not have a static name here? Plus is this relative "path" okay?
-	scene_file = 'nextgame.pck'
+	resource_pack_file = 'nextgame.pck'
 	
 	http_request_node = HTTPRequest.new()
 	http_request_node.connect("request_completed", self, "_http_request_next_completed")
@@ -18,7 +19,13 @@ func _ready():
 
 # Call this from wherever
 func load_next():
-	var error = http_request_node.request("http://localhost:8082/godot-delivery/next-game")
+	var platform = "windowsdesktop"
+	if OS.get_name() == "X11":
+		platform = "linuxx11"
+		
+	var game_name = ProjectSettings["application/config/name"]
+		
+	var error = http_request_node.request("http://localhost:8082/godot-delivery/next-game?platform=" + platform + "&gamename=" + game_name)
 	if error != OK:
 		push_error("An error occurred in the HTTP request to load the next level identifier.")
 
@@ -27,9 +34,11 @@ func _http_request_next_completed(result, response_code, headers, body):
 		push_error("Next level loader got BadRequest")
 		return
 	
-	var download_url = body.get_string_from_utf8()
+	var response = parse_json(body.get_string_from_utf8())
+	var download_url = response["download_url"]
+	main_scene_file = response["main_scene"]
 	
-	http_downloader_node.download_file = scene_file
+	http_downloader_node.download_file = resource_pack_file
 	http_downloader_node.request(download_url)
 
 func _http_request_download_completed(result, response_code, headers, body):
@@ -37,6 +46,11 @@ func _http_request_download_completed(result, response_code, headers, body):
 		push_error("Next level downloader got BadRequest")
 		return
 		
-	var error = get_tree().change_scene(scene_file)
+	var success = ProjectSettings.load_resource_pack(resource_pack_file)
+	if !success:
+		push_error("Unable to load resource pack")
+		return
+		
+	var error = get_tree().change_scene(main_scene_file)
 	if error != OK:
 		push_error("Unable to change to new scene")
